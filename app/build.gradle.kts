181598fe191a66fb693aee188dead654b1860167
yutong119 <yutong.ji@my.jcu.edu.au>
2026-08-02 21:53:49 +0800

    fix: update compile SDK for AndroidX dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 android {
     namespace = "com.example.dinopath"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
