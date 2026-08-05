5b138423f3cdb1df358472bdcf6a1c3b862ce70d
yutong119 <yutong.ji@my.jcu.edu.au>
2026-08-05 20:33:46 +0800

    D6-04A feat: add favourite specimen Room storage

--- app/src/main/java/com/example/dinopath/data/local/dao/DinoPathDao.kt
+++ app/src/main/java/com/example/dinopath/data/local/dao/DinoPathDao.kt
@@ -137,27 +137,4 @@ interface DinoPathDao {
         specimenId: String,
     )
 
-    @Query(
-        """
-    SELECT * FROM favourite_specimens
-    ORDER BY savedAt DESC
-    """
-    )
-    fun observeFavouriteSpecimens():
-            Flow<List<FavouriteSpecimenEntity>>
-
-    @Upsert
-    suspend fun upsertFavourite(
-        specimen: FavouriteSpecimenEntity,
-    )
-
-    @Query(
-        """
-    DELETE FROM favourite_specimens
-    WHERE specimenId = :specimenId
-    """
-    )
-    suspend fun deleteFavourite(
-        specimenId: String,
-    )
 }
